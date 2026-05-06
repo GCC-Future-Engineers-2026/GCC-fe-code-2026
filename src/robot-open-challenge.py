4578dabcab57a63bc2fd2e4a9ff4a544e88bdf0c
GCC-Future-Engineers-2026 <Thomjam2k@gmail.com>
2026-05-06 18:43:57 +0800

    maximize speed and improve centering using gyro sensor

--- src/robot-open-challenge.py
+++ src/robot-open-challenge.py
@@ -4,6 +4,7 @@ from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
 from pybricks.robotics import Car
+from pupremote_hub import PUPRemoteHub
 
 
 
@@ -19,11 +20,21 @@ eyesR = UltrasonicSensor(Port.F)
 eyesL = UltrasonicSensor(Port.E)
 eyesF = UltrasonicSensor(Port.A)
 
+# OpenMV camera
+p = PUPRemoteHub(Port.D)
+p.add_command("msg", to_hub_fmt="repr", from_hub_fmt="repr")
+p.add_channel('color', to_hub_fmt='f')  # char (0: none, 1: green, 2: red)
+p.add_channel('bl_x', to_hub_fmt='f')  # center x of rectangle
+p.add_channel('bl_y', to_hub_fmt='f')  # center y of rectangle
+p.add_channel('cor', to_hub_fmt='f')  # presence of corner
+# p.add_channel('lba', to_hub_fmt='b')  # integer presence of red block at left bottom area
+# p.add_channel('rba', to_hub_fmt='b')  # integer presence of green block at right bottom area
+
 hub.imu.reset_heading(0)
-wait(1000)
+wait(500)
 
 car = Car(steer, rear, 100)
-wait(1000)
+wait(500)
 
 # error => eyes.distance() - target
 
@@ -31,18 +42,25 @@ targetDist = 100
 sum_error = 0
 prev_error = 0
 
-kp = 0.035
-ki = 0.00001
-kd = 1000000.0
+sum_Gerror = 0
+prev_Gerror = 0
+sum_Rerror = 0 
+prev_Rerror = 0
+
+sumS_error = 0
+prevS_error = 0
 
 distList = [0, 0, 0]
 distListR = [0, 0, 0]
 distListL = [0, 0, 0]
 distListF = [0, 0, 0]
 
-max_steer = 20
-num_turn = 1
+max_steer = 20 
+# num_turn = 1
 direction = 0
+
+drive_power = 0.0
+compensation = 0.0
 
 def getMedian():
     i = 0
@@ -104,92 +122,38 @@ def getMedianF(samples):
     else:
         return (distListF[mid-1] + distListF[mid]) / 2
 
+def UltrasonicPID_2Sensor(kp=0.05, ki=0.000001, kd=100000.0, gyro_angle_correct=25):
 
-def UltrasonicPID_RSensor():
-
-    while True:
-
-        # error = getMedianR(3) - getMedianL(3)
-        error = getMedian(3) - targetDist
-        
-        #print("error: ", error)
-
-        if error > 500:  # corner
-            car.drive_speed(0)
-            hub.speaker.beep()
-            car.steer(0)
-            # car.drive_speed(200)
-            # wait(100)
-            # car.drive_speed(0)
-            car.steer(max_steer)
-            while getMedian(3) > targetDist:
-                car.drive_speed(300)
-            
-            car.steer(0)
-            car.drive_speed(0)
-            wait(250)
-
-
-        prop = kp * error # proportional
-        
-        sum_error += error
-        integral = ki * sum_error # integral
-
-        if integral > 10.0:
-            integral = 10.0
-
-        derivative = kd * (error - prev_error) # derivative
-
-        compensation = prop + integral + derivative
-
-        prev_error = error
-
-        # negative steer = steer to the right 
-        # positive steer = steer to the left
-        
-        if compensation > max_steer:
-            compensation = max_steer
-        elif compensation < -max_steer:
-            compensation = -max_steer
-            
-        car.steer(compensation)
-
-        # drive power range => 300 - 800
-        # compensation range => 0 - max_compensation
-        maxP = 700
-        minP = 200
-        max_error = 2000 - targetDist
-        
-        drive_power = -(maxP - minP) * (error - max_error) / max_error + minP   
-        # print("drive_power: ", drive_power)
-        car.drive_speed(drive_power)
-        wait(10)
-
-
-def UltrasonicPID_2Sensor():
-
-    global kp, ki, kd, sum_error, prev_error, max_steer, num_turn, direction
+    global sum_error, prev_error, max_steer, direction, corner
+    num_turn = 1
 
     drive_power = 0.0
     compensation = 0.0
     
     while num_turn <= 12:
 
+        corner = p.call('cor')  # 0 -> no corner or 1 -> corner 
+
         error = getMedianR(3) - getMedianL(3)
-        #print("error: ", error)
+        # print("error: ", error)
+        # print("prev_error: ", prev_error)
 
-        while hub.imu.heading() > 20 + (90 * (num_turn-1)):
-            car.steer(-30)
-            car.drive_speed(600)
-        
-        while hub.imu.heading() < -20 - (90 * (num_turn-1)):
-            car.steer(30)
-            car.drive_speed(600)
-
-        if getMedianF(3) < 275:  # corner wall
+        if hub.imu.heading() > gyro_angle_correct + (90 * (num_turn-1)):
+            while hub.imu.heading() > gyro_angle_correct//2 + (90 * (num_turn-1)) and corner == 0:
+                corner = p.call('cor')  # 0 -> no corner or 1 -> corner
+                car.steer(-30)
+                car.drive_speed(800)
+            
+        if hub.imu.heading() < -gyro_angle_correct - (90 * (num_turn-1)):
+            while hub.imu.heading() < -gyro_angle_correct//2 -(90 * (num_turn-1)) and corner == 0:
+                corner = p.call('cor')  # 0 -> no corner or 1 -> corner
+                car.steer(30)
+                car.drive_speed(800)
+            
+        if getMedianF(3) < 450 and corner == 1:  # corner wall
             car.drive_speed(0)
             hub.speaker.beep()
-            wait(250)
+            # wait(250)
 
             if direction == 0:
                 if getMedianL(3) > getMedianR(3): # left corner turn
@@ -203,14 +167,14 @@ def UltrasonicPID_2Sensor():
             elif direction == 1:
                 car.steer(75)   # right turn
             
-            while abs(hub.imu.heading()) < (90 * num_turn):
-                car.drive_speed(400)
+            while abs(hub.imu.heading()) < (90 * num_turn - 10):
+                car.drive_speed(800)
 
             num_turn += 1
             # print("num_turn: ", num_turn)
             car.drive_speed(0)
             car.steer(0)
-            wait(500)
+            # wait(250)
 
 
         prop = kp * error # proportional
@@ -236,11 +200,12 @@ def UltrasonicPID_2Sensor():
             compensation = -max_steer
             
         car.steer(compensation)
+        # print("compensation: ", compensation)
 
         # drive power range => 300 - 800
         # compensation range => 0 - max_compensation
         maxP = 1100
-        minP = 800
+        minP = 900
         max_error = 2000
         
         if error > max_error:
@@ -253,21 +218,12 @@ def UltrasonicPID_2Sensor():
         car.drive_speed(drive_power)
         wait(10)
 
+    # finishing
+    rear.reset_angle(0)
+    while rear.angle() < 2250:
+        UltrasonicPID_2Sensor_C(num_turn=13, kp=0.05, ki=0.000001, kd=10000, max_steer=20, gyro_angle_correct=25)
+
     car.drive_speed(0)
 
-# UltrasonicPID_RSensor()
-UltrasonicPID_2Sensor()
-
-# while True:
-    # print("LeftUS: ", getMedianL(3))
-    # print("RightUS: ", getMedianR(3))
-    # print("Error: ", getMedianR(3) - getMedianL(3))
-    # print("LeftUS: ", eyesL.distance())
-    # print("RightUS: ", eyesR.distance())
-    # print("Error: ", eyesR.distance() - eyesL.distance())
-    # wait(1000)
-
-
-
-
-
+# open challenge
+UltrasonicPID_2Sensor(kp=0.05, ki=0.000001, kd=100000.0, gyro_angle_correct=25)
